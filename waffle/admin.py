11cedaf1c8603299eb0e6bd2755a4e6d2af4aaeb
James Socol <james@mozilla.com>
2011-03-16 17:23:03 -0400

    Add enable/disable admin actions.

--- waffle/admin.py
+++ waffle/admin.py
@@ -3,7 +3,18 @@ from django.contrib import admin
 from waffle.models import Flag
 
 
+def enable_for_all(ma, request, qs):
+    qs.update(everyone=True)
+enable_for_all.short_description = 'Enable selected flags for everyone.'
+
+
+def disable_for_all(ma, request, qs):
+    qs.update(everyone=False)
+disable_for_all.short_description = 'Disable selected flags for everyone.'
+
+
 class FlagAdmin(admin.ModelAdmin):
+    actions = [enable_for_all, disable_for_all]
     list_display = ('name', 'everyone', 'percent', 'superusers', 'staff',
                     'authenticated')
     list_filter = ('everyone', 'superusers', 'staff', 'authenticated')

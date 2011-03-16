ad0f820b1fe93a2dfdcc7c1bbc2f8ae784459242
James Socol <james@mozilla.com>
2011-03-16 17:24:44 -0400

    PyPI version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-waffle',
-    version='0.2',
+    version='0.2.1',
     description='A feature flipper for Django.',
     long_description=open('README.rst').read(),
     author='James Socol',

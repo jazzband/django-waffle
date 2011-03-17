3f7e51e389fae5afd51d80eee206cfbe94a7ea98
James Socol <james@mozilla.com>
2011-03-17 18:26:58 -0400

    More l33t JS (thanks to Ricky!)

--- waffle/templates/waffle/waffle.js
+++ waffle/templates/waffle/waffle.js
@@ -3,9 +3,6 @@
         {% for flag, value in flags %}'{{ flag }}': {% if value %}true{% else %}false{% endif %}{% if not forloop.last %},{% endif %}{% endfor %}
     };
     window.waffle = function waffle(flag) {
-        if (typeof FLAGS[flag] == 'undefined') {
-            return false;
-        }
-        return FLAGS[flag];
+        return !!FLAGS[flag];
     };
 })();

f58714223a8d026ce26365bf8b4aa701a04bd109
James Socol <james@mozilla.com>
2011-03-31 17:29:24 -0400

    Version bump.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-waffle',
-    version='0.3',
+    version='0.4',
     description='A feature flipper for Django.',
     long_description=open('README.rst').read(),
     author='James Socol',

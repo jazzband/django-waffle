678d69cd8fbc40a50c862629ea60679deb70306c
Ricky Rosario <rickyrosario@gmail.com>
2011-04-01 11:53:44 -0400

    Delete from cache on model delete, listen for m2m_changed events, empty list is falsy so need to compare to None.

--- waffle/__init__.py
+++ waffle/__init__.py
@@ -3,7 +3,7 @@ import random
 
 from django.conf import settings
 from django.core.cache import cache
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete, m2m_changed
 
 from waffle.models import Flag, Switch
 
@@ -43,13 +43,17 @@ def flag_is_active(request, flag_name):
     if flag.superusers and user.is_superuser:
         return True
 
-    flag_users = (cache.get(FLAG_USERS_CACHE_KEY.format(n=flag.name)) or 
-                  flag.users.all())
+    flag_users = cache.get(FLAG_USERS_CACHE_KEY.format(n=flag.name))
+    if flag_users == None:
+        flag_users = flag.users.all()
+        cache_flag(instance=flag)
     if user in flag_users:
         return True
 
-    flag_groups = (cache.get(FLAG_GROUPS_CACHE_KEY.format(n=flag.name)) or 
-                  flag.groups.all())
+    flag_groups = cache.get(FLAG_GROUPS_CACHE_KEY.format(n=flag.name))
+    if flag_groups == None:
+        flag_groups = flag.groups.all()
+        cache_flag(instance=flag)
     for group in flag_groups:
         if group in user.groups.all():
             return True
@@ -86,12 +90,28 @@ def switch_is_active(switch_name):
 
 
 def cache_flag(**kwargs):
-    flag = kwargs.get('instance')
-    cache.set(FLAG_CACHE_KEY.format(n=flag.name), flag)
-    cache.set(FLAG_USERS_CACHE_KEY.format(n=flag.name), flag.users.all())
-    cache.set(FLAG_GROUPS_CACHE_KEY.format(n=flag.name), flag.groups.all())
+    action = kwargs.get('action', None)
+    # action is included for m2m_changed signal. Only cache on the post_*.
+    if not action or action in ['post_add', 'post_remove', 'post_clear']:
+        f = kwargs.get('instance')
+        cache.set(FLAG_CACHE_KEY.format(n=f.name), f)
+        cache.set(FLAG_USERS_CACHE_KEY.format(n=f.name), f.users.all())
+        cache.set(FLAG_GROUPS_CACHE_KEY.format(n=f.name), f.groups.all())
 
 post_save.connect(cache_flag, sender=Flag, dispatch_uid='cache_flag')
+m2m_changed.connect(cache_flag, sender=Flag.users.through,
+                    dispatch_uid='cache_flag_users')
+m2m_changed.connect(cache_flag, sender=Flag.groups.through,
+                    dispatch_uid='cache_flag_groups')
+
+
+def uncache_flag(**kwargs):
+    flag = kwargs.get('instance')
+    cache.delete_many([FLAG_CACHE_KEY.format(n=flag.name),
+                       FLAG_USERS_CACHE_KEY.format(n=flag.name),
+                       FLAG_GROUPS_CACHE_KEY.format(n=flag.name)])
+
+post_delete.connect(uncache_flag, sender=Flag, dispatch_uid='uncache_flag')
 
 
 def cache_switch(**kwargs):
@@ -99,3 +119,11 @@ def cache_switch(**kwargs):
     cache.set(SWITCH_CACHE_KEY.format(n=switch.name), switch)
 
 post_save.connect(cache_switch, sender=Switch, dispatch_uid='cache_switch')
+
+
+def ucache_switch(**kwargs):
+    switch = kwargs.get('instance')
+    cache.delete(SWITCH_CACHE_KEY.format(n=switch.name))
+
+post_delete.connect(ucache_switch, sender=Switch, dispatch_uid='uncache_switch')
+

696a94a2c7b4dbb2fc57cdab9dc7f64ffdaa6c96
Timothée Peignier <timothee.peignier@tryphon.org>
2011-11-24 18:24:10 +0100

    add 'switch' command

---
+++ waffle/management/commands/switch.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from waffle.models import Switch
+
+
+class Command(BaseCommand):
+    help = "Activate or deactivate a switch."
+    args = "<switch_name> <on/off>"
+
+    def handle(self, switch_name=None, state=None, *args, **options):
+        if not (switch_name and state):
+            raise CommandError('You need to specify a switch name and his state.')
+
+        if not state in ["on", "off"]:
+            raise CommandError('You need to specify state of switch with "on" or "off"')
+
+        try:
+            switch = Switch.objects.get(name=switch_name)
+            switch.active = state == "on"
+            switch.save()
+        except Switch.DoesNotExist:
+            raise CommandError('THis switch doesn\'t exists')

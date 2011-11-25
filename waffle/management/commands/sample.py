2d13535bf06646fb695ec1e62b6fc5e0d99677d5
Timothée Peignier <timothee.peignier@tryphon.org>
2011-11-25 12:04:08 +0100

    add 'sample' command

---
+++ waffle/management/commands/sample.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from waffle.models import Sample
+
+
+class Command(BaseCommand):
+    help = "Change percentage of a sample."
+    args = "<sample_name> <percent>"
+
+    def handle(self, sample_name=None, percent=None, *args, **options):
+        if not (sample_name and percent):
+            raise CommandError('You need to specify a sample name and his percentage.')
+
+        try:
+            percent = float(percent)
+            if not (0.0 <= percent <= 100.0):
+                raise ValueError()
+        except ValueError:
+            raise CommandError('You need to a valid percentage value')
+
+        try:
+            sample = Sample.objects.get(name=sample_name)
+            sample.percent = percent
+            sample.save()
+        except Sample.DoesNotExist:
+            raise CommandError('This sample doesn\'t exist')

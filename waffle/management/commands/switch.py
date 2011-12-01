16ff7d93d2ebd92120c5bb7bf4b6e5c266b2ce2e
Timothée Peignier <timothee.peignier@tryphon.org>
2011-12-01 18:52:17 +0100

    add list to switch command

--- waffle/management/commands/switch.py
+++ waffle/management/commands/switch.py
@@ -1,13 +1,28 @@
+from optparse import make_option
+
 from django.core.management.base import BaseCommand, CommandError
 
 from waffle.models import Switch
 
 
 class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('-l', '--list',
+            action='store_true', dest='list_switch', default=False,
+            help="List existing switchs."),
+    )
     help = "Activate or deactivate a switch."
     args = "<switch_name> <on/off>"
 
     def handle(self, switch_name=None, state=None, *args, **options):
+        list_switch = options['list_switch']
+
+        if list_switch:
+            print "switchs :"
+            for switch in Switch.objects.iterator():
+                print switch.name, "on" if switch.active else "off"
+            return
+
         if not (switch_name and state):
             raise CommandError('You need to specify a switch name and his state.')
 

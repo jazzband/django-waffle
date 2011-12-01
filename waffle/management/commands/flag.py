e8aea391d453259dad62f3c8efb13219ab211e00
Timothée Peignier <timothee.peignier@tryphon.org>
2011-12-01 18:52:17 +0100

    add list to switch command

---
+++ waffle/management/commands/flag.py
@@ -0,0 +1,33 @@
+from optparse import make_option
+
+from django.core.management.base import BaseCommand, CommandError
+
+from waffle.models import Flag
+
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--everyone',
+            action='',
+            dest='rollout_activate',
+            help="Activate given target for feature."
+        ),
+        make_option('--deactivate',
+            action='store_false',
+            dest='rollout_activate',
+            help="Deactivate given target for feature."
+        )
+    )
+    help = "Modify a flag."
+    args = "<flag_name>"
+
+    def handle(self, flag_name=None, *args, **options):
+        if not flag_name:
+            raise CommandError('You need to specify a flag name.')
+
+        try:
+            flag = Flag.objects.get(name=flag_name)
+            setattr(flag, attribute, value)
+            flag.save()
+        except Flag.DoesNotExist:
+            raise CommandError('This flag doesn\'t exist')

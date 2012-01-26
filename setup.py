acc8e4adb41e7713be9778460fc3e99e034b7511
James Socol <james@mozilla.com>
2012-01-26 13:54:29 -0500

    Version bump for the recent changes.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-waffle',
-    version='0.7.6',
+    version='0.8.0',
     description='A feature flipper for Django.',
     long_description=open('README.rst').read(),
     author='James Socol',
--- waffle/__init__.py
+++ waffle/__init__.py
@@ -8,7 +8,7 @@ from django.db.models.signals import post_save, post_delete, m2m_changed
 from waffle.models import Flag, Sample, Switch
 
 
-VERSION = (0, 7, 6)
+VERSION = (0, 8, 0)
 __version__ = '.'.join(map(str, VERSION))
 
 

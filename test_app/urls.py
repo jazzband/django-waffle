f2109a486b3459a3fbf4e5e7db92780f1765a5a8
James Socol <james@mozilla.com>
2012-08-07 15:09:56 -0400

    Use new URLs module in test_app.

--- test_app/urls.py
+++ test_app/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.http import HttpResponseNotFound, HttpResponseServerError
 
 from test_app import views
-from waffle.views import wafflejs
 
 
 handler404 = lambda r: HttpResponseNotFound()
@@ -13,10 +12,10 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^flag_in_view', views.flag_in_view, name='flag_in_view'),
-    url(r'^wafflejs$', wafflejs, name='wafflejs'),
     url(r'^switch-on', views.switched_view),
     url(r'^switch-off', views.switched_off_view),
     url(r'^flag-on', views.flagged_view),
     url(r'^flag-off', views.flagged_off_view),
+    (r'^', include('waffle.urls')),
     (r'^admin/', include(admin.site.urls))
 )

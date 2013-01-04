19b27a08c41a0004f0b96eb73f8c0cb224c76670
James Socol <me@jamessocol.com>
2013-01-04 12:07:22 -0500

    PEP8 and DRYer.

--- waffle/__init__.py
+++ waffle/__init__.py
@@ -14,16 +14,22 @@ __version__ = '.'.join(map(str, VERSION))
 
 
 CACHE_PREFIX = getattr(settings, 'WAFFLE_CACHE_PREFIX', u'waffle:')
-FLAG_CACHE_KEY = CACHE_PREFIX + u'flag:{n}'
-FLAGS_ALL_CACHE_KEY = CACHE_PREFIX + u'flags:all'
-FLAG_USERS_CACHE_KEY = CACHE_PREFIX + u'flag:{n}:users'
-FLAG_GROUPS_CACHE_KEY = CACHE_PREFIX + u'flag:{n}:groups'
-SAMPLE_CACHE_KEY = CACHE_PREFIX + u'sample:{n}'
-SAMPLES_ALL_CACHE_KEY = CACHE_PREFIX + u'samples:all'
-SWITCH_CACHE_KEY = CACHE_PREFIX + u'switch:{n}'
-SWITCHES_ALL_CACHE_KEY = CACHE_PREFIX + u'switches:all'
+FLAG_CACHE_KEY = u'flag:%s'
+FLAGS_ALL_CACHE_KEY = u'flags:all'
+FLAG_USERS_CACHE_KEY = u'flag:%s:users'
+FLAG_GROUPS_CACHE_KEY = u'flag:%s:groups'
+SAMPLE_CACHE_KEY = u'sample:%s'
+SAMPLES_ALL_CACHE_KEY = u'samples:all'
+SWITCH_CACHE_KEY = u'switch:%s'
+SWITCHES_ALL_CACHE_KEY = u'switches:all'
 COOKIE_NAME = getattr(settings, 'WAFFLE_COOKIE', 'dwf_%s')
 TEST_COOKIE_NAME = getattr(settings, 'WAFFLE_TESTING_COOKIE', 'dwft_%s')
+
+
+def keyfmt(k, v=None):
+    if v is None:
+        return CACHE_PREFIX + k
+    return CACHE_PREFIX + hashlib.md5(k % v).hexdigest()
 
 
 class DoesNotExist(object):
@@ -41,7 +47,7 @@ def set_flag(request, flag_name, active=True, session_only=False):
 
 
 def flag_is_active(request, flag_name):
-    flag = cache.get(FLAG_CACHE_KEY.format(n=hashlib.md5(flag_name).hexdigest()))
+    flag = cache.get(keyfmt(FLAG_CACHE_KEY, flag_name))
     if flag is None:
         try:
             flag = Flag.objects.get(name=flag_name)
@@ -86,14 +92,14 @@ def flag_is_active(request, flag_name):
                 request.LANGUAGE_CODE in languages):
             return True
 
-    flag_users = cache.get(FLAG_USERS_CACHE_KEY.format(n=hashlib.md5(flag.name).hexdigest()))
+    flag_users = cache.get(keyfmt(FLAG_USERS_CACHE_KEY, flag.name))
     if flag_users is None:
         flag_users = flag.users.all()
         cache_flag(instance=flag)
     if user in flag_users:
         return True
 
-    flag_groups = cache.get(FLAG_GROUPS_CACHE_KEY.format(n=hashlib.md5(flag.name).hexdigest()))
+    flag_groups = cache.get(keyfmt(FLAG_GROUPS_CACHE_KEY, flag.name))
     if flag_groups is None:
         flag_groups = flag.groups.all()
         cache_flag(instance=flag)
@@ -123,7 +129,7 @@ def flag_is_active(request, flag_name):
 
 
 def switch_is_active(switch_name):
-    switch = cache.get(SWITCH_CACHE_KEY.format(n=hashlib.md5(switch_name).hexdigest()))
+    switch = cache.get(keyfmt(SWITCH_CACHE_KEY, switch_name))
     if switch is None:
         try:
             switch = Switch.objects.get(name=switch_name)
@@ -136,7 +142,7 @@ def switch_is_active(switch_name):
 
 
 def sample_is_active(sample_name):
-    sample = cache.get(SAMPLE_CACHE_KEY.format(n=hashlib.md5(sample_name).hexdigest()))
+    sample = cache.get(keyfmt(SAMPLE_CACHE_KEY, sample_name))
     if sample is None:
         try:
             sample = Sample.objects.get(name=sample_name)
@@ -152,18 +158,18 @@ def cache_flag(**kwargs):
     # action is included for m2m_changed signal. Only cache on the post_*.
     if not action or action in ['post_add', 'post_remove', 'post_clear']:
         f = kwargs.get('instance')
-        cache.add(FLAG_CACHE_KEY.format(n=hashlib.md5(f.name).hexdigest()), f)
-        cache.add(FLAG_USERS_CACHE_KEY.format(n=hashlib.md5(f.name).hexdigest()), f.users.all())
-        cache.add(FLAG_GROUPS_CACHE_KEY.format(n=hashlib.md5(f.name).hexdigest()), f.groups.all())
+        cache.add(keyfmt(FLAG_CACHE_KEY, f.name), f)
+        cache.add(keyfmt(FLAG_USERS_CACHE_KEY, f.name), f.users.all())
+        cache.add(keyfmt(FLAG_GROUPS_CACHE_KEY, f.name), f.groups.all())
 
 
 def uncache_flag(**kwargs):
     flag = kwargs.get('instance')
     data = {
-        FLAG_CACHE_KEY.format(n=hashlib.md5(flag.name).hexdigest()): None,
-        FLAG_USERS_CACHE_KEY.format(n=hashlib.md5(flag.name).hexdigest()): None,
-        FLAG_GROUPS_CACHE_KEY.format(n=hashlib.md5(flag.name).hexdigest()): None,
-        FLAGS_ALL_CACHE_KEY: None
+        keyfmt(FLAG_CACHE_KEY, flag.name): None,
+        keyfmt(FLAG_USERS_CACHE_KEY, flag.name): None,
+        keyfmt(FLAG_GROUPS_CACHE_KEY, flag.name): None,
+        keyfmt(FLAGS_ALL_CACHE_KEY): None
     }
     cache.set_many(data, 5)
 
@@ -177,13 +183,13 @@ m2m_changed.connect(uncache_flag, sender=Flag.groups.through,
 
 def cache_sample(**kwargs):
     sample = kwargs.get('instance')
-    cache.add(SAMPLE_CACHE_KEY.format(n=hashlib.md5(sample.name).hexdigest()), sample)
+    cache.add(keyfmt(SAMPLE_CACHE_KEY, sample.name), sample)
 
 
 def uncache_sample(**kwargs):
     sample = kwargs.get('instance')
-    cache.set(SAMPLE_CACHE_KEY.format(n=hashlib.md5(sample.name).hexdigest()), None, 5)
-    cache.set(SAMPLES_ALL_CACHE_KEY, None, 5)
+    cache.set(keyfmt(SAMPLE_CACHE_KEY, sample.name), None, 5)
+    cache.set(keyfmt(SAMPLES_ALL_CACHE_KEY), None, 5)
 
 post_save.connect(uncache_sample, sender=Sample, dispatch_uid='save_sample')
 post_delete.connect(uncache_sample, sender=Sample,
@@ -192,13 +198,13 @@ post_delete.connect(uncache_sample, sender=Sample,
 
 def cache_switch(**kwargs):
     switch = kwargs.get('instance')
-    cache.add(SWITCH_CACHE_KEY.format(n=hashlib.md5(switch.name).hexdigest()), switch)
+    cache.add(keyfmt(SWITCH_CACHE_KEY, switch.name), switch)
 
 
 def uncache_switch(**kwargs):
     switch = kwargs.get('instance')
-    cache.set(SWITCH_CACHE_KEY.format(n=hashlib.md5(switch.name).hexdigest()), None, 5)
-    cache.set(SWITCHES_ALL_CACHE_KEY, None, 5)
+    cache.set(keyfmt(SWITCH_CACHE_KEY, switch.name), None, 5)
+    cache.set(keyfmt(SWITCHES_ALL_CACHE_KEY), None, 5)
 
 post_delete.connect(uncache_switch, sender=Switch,
                     dispatch_uid='delete_switch')

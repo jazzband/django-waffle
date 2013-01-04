bd656ed392a7921e96f7c0f9e9cc2569a77de674
James Socol <me@jamessocol.com>
2013-01-04 12:50:27 -0500

    Version bump, 0.9

--- waffle/__init__.py
+++ waffle/__init__.py
@@ -9,7 +9,7 @@ from django.db.models.signals import post_save, post_delete, m2m_changed
 from waffle.models import Flag, Sample, Switch
 
 
-VERSION = (0, 8, 1)
+VERSION = (0, 9, 0)
 __version__ = '.'.join(map(str, VERSION))
 
 

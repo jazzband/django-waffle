753674384efc7ab75bd272569652a5ae32b64c4f
Paul Watts <paulcwatts@gmail.com>
2013-07-02 10:12:24 -0700

    Fix caching/invalidation of 'all' lists
    The wafflejs view wasn't storing the ALL lists using keyfmt(), so the
    post_save/post_delete signals that were using keyfmt weren't
    invalidating the cache.

--- waffle/tests/test_views.py
+++ waffle/tests/test_views.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import reverse
 
 from nose.tools import eq_
 
+from waffle.models import Flag, Sample, Switch
 from waffle.tests.base import TestCase
 
 
@@ -11,3 +12,42 @@ class WaffleViewTests(TestCase):
         eq_(200, response.status_code)
         eq_('application/x-javascript', response['content-type'])
         eq_('max-age=0', response['cache-control'])
+
+    def test_flush_all_flags(self):
+        """Test the 'FLAGS_ALL' list gets invalidated correctly."""
+        Flag.objects.create(name='myflag1', everyone=True)
+        response = self.client.get(reverse('wafflejs'))
+        eq_(200, response.status_code)
+        assert ('myflag1', True) in response.context['flags']
+
+        Flag.objects.create(name='myflag2', everyone=True)
+
+        response = self.client.get(reverse('wafflejs'))
+        eq_(200, response.status_code)
+        assert ('myflag2', True) in response.context['flags']
+
+    def test_flush_all_switches(self):
+        """Test the 'SWITCHES_ALL' list gets invalidated correctly."""
+        switch = Switch.objects.create(name='myswitch', active=True)
+        response = self.client.get(reverse('wafflejs'))
+        eq_(200, response.status_code)
+        assert ('myswitch', True) in response.context['switches']
+
+        switch.active = False
+        switch.save()
+        response = self.client.get(reverse('wafflejs'))
+        eq_(200, response.status_code)
+        assert ('myswitch', False) in response.context['switches']
+
+    def test_flush_all_samples(self):
+        """Test the 'SAMPLES_ALL' list gets invalidates correctly."""
+        Sample.objects.create(name='sample1', percent='100.0')
+        response = self.client.get(reverse('wafflejs'))
+        eq_(200, response.status_code)
+        assert ('sample1', True) in response.context['samples']
+
+        Sample.objects.create(name='sample2', percent='100.0')
+
+        response = self.client.get(reverse('wafflejs'))
+        eq_(200, response.status_code)
+        assert ('sample2', True) in response.context['samples']

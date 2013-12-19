d2a4a4a8ec8ee159b28fa4b90bfb0739b74f3166
Vail Gold <vail130@gmail.com>
2013-12-19 16:21:31 -0500

    Removed redundant code.

--- waffle/compat.py
+++ waffle/compat.py
@@ -5,12 +5,8 @@ __all__ = ['User', 'get_user_model']
 # Django 1.5+ compatibility
 if django.VERSION >= (1, 5):
     from django.conf import settings
-    from django.contrib.auth import get_user_model as get_auth_user_model
-
+    from django.contrib.auth import get_user_model
     User = settings.AUTH_USER_MODEL if hasattr(settings, 'AUTH_USER_MODEL') else 'auth.User'
-
-    def get_user_model():
-        return get_auth_user_model()
 
 else:
     from django.contrib.auth.models import User

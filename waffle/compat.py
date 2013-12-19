f73d542b4a36ebceea80f489f9ac0169b2110fa3
Vail Gold <vail130@gmail.com>
2013-12-19 10:06:49 -0500

    Fixed initial south migration.
    Initial south migration needs a reference to the actual User model,
    so I added a get_user_model function importable from waffle.compat
    to allow the migration to get a reference to the model.

--- waffle/compat.py
+++ waffle/compat.py
@@ -5,6 +5,15 @@ __all__ = ['User']
 # Django 1.5+ compatibility
 if django.VERSION >= (1, 5):
     from django.conf import settings
-    User = settings.AUTH_USER_MODEL
+    from django.contrib.auth import get_user_model as get_auth_user_model
+
+    User = settings.AUTH_USER_MODEL if hasattr(settings, 'AUTH_USER_MODEL') else 'auth.User'
+
+    def get_user_model():
+        return get_auth_user_model()
+
 else:
     from django.contrib.auth.models import User
+
+    def get_user_model():
+        return User

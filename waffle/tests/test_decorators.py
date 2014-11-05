cb8a455937f8062874c1256c9d2f2b4e116d12e4
Florent Messa <florent.messa@gmail.com>
2014-11-05 17:17:20 +0100

    Remove nose dependency everywhere Remove nose related in requirements Add django-discover-runner for compatibility with older django versions

--- waffle/tests/test_decorators.py
+++ waffle/tests/test_decorators.py
@@ -1,5 +1,3 @@
-from nose.tools import eq_
-
 from waffle.models import Flag, Switch
 from waffle.tests.base import TestCase
 
@@ -7,28 +5,28 @@ from waffle.tests.base import TestCase
 class DecoratorTests(TestCase):
     def test_flag_must_be_active(self):
         resp = self.client.get('/flag-on')
-        eq_(404, resp.status_code)
+        self.assertEqual(404, resp.status_code)
         Flag.objects.create(name='foo', everyone=True)
         resp = self.client.get('/flag-on')
-        eq_(200, resp.status_code)
+        self.assertEqual(200, resp.status_code)
 
     def test_flag_must_be_inactive(self):
         resp = self.client.get('/flag-off')
-        eq_(200, resp.status_code)
+        self.assertEqual(200, resp.status_code)
         Flag.objects.create(name='foo', everyone=True)
         resp = self.client.get('/flag-off')
-        eq_(404, resp.status_code)
+        self.assertEqual(404, resp.status_code)
 
     def test_switch_must_be_active(self):
         resp = self.client.get('/switch-on')
-        eq_(404, resp.status_code)
+        self.assertEqual(404, resp.status_code)
         Switch.objects.create(name='foo', active=True)
         resp = self.client.get('/switch-on')
-        eq_(200, resp.status_code)
+        self.assertEqual(200, resp.status_code)
 
     def test_switch_must_be_inactive(self):
         resp = self.client.get('/switch-off')
-        eq_(200, resp.status_code)
+        self.assertEqual(200, resp.status_code)
         Switch.objects.create(name='foo', active=True)
         resp = self.client.get('/switch-off')
-        eq_(404, resp.status_code)
+        self.assertEqual(404, resp.status_code)

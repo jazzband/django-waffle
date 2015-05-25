bc2064b37ac9565da7a259ac07cf566b113adea4
Mauricio de Abreu Antunes <mauricio.abreua@gmail.com>
2015-05-25 20:49:10 -0300

    Reseted South migrations
    Since any South migrations are not working
    this patch fixes Django 1.6 compatibility (even breaking upgrade path).

--- waffle/south_migrations/0001_initial.py
+++ waffle/south_migrations/0001_initial.py
@@ -1,134 +1,162 @@
-# encoding: utf-8
-import datetime
-import django
-
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
-
-
-# Django 1.5+ compatibility
-if django.VERSION >= (1, 5):
-    from django.contrib.auth import get_user_model
-else:
-    from django.contrib.auth.models import User
-
-    def get_user_model():
-        return User
+from django.conf import settings
 
 
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        
         # Adding model 'Flag'
-        db.create_table('waffle_flag', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'waffle_flag', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=100)),
             ('everyone', self.gf('django.db.models.fields.NullBooleanField')(null=True, blank=True)),
             ('percent', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=3, decimal_places=1, blank=True)),
+            ('testing', self.gf('django.db.models.fields.BooleanField')(default=False)),
             ('superusers', self.gf('django.db.models.fields.BooleanField')(default=True)),
             ('staff', self.gf('django.db.models.fields.BooleanField')(default=False)),
             ('authenticated', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('languages', self.gf('django.db.models.fields.TextField')(default='', blank=True)),
             ('rollout', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('note', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, db_index=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
         ))
-        db.send_create_signal('waffle', ['Flag'])
+        db.send_create_signal(u'waffle', ['Flag'])
 
         # Adding M2M table for field groups on 'Flag'
-        db.create_table('waffle_flag_groups', (
+        m2m_table_name = db.shorten_name(u'waffle_flag_groups')
+        db.create_table(m2m_table_name, (
             ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('flag', models.ForeignKey(orm['waffle.flag'], null=False)),
-            ('group', models.ForeignKey(orm['auth.group'], null=False))
+            ('flag', models.ForeignKey(orm[u'waffle.flag'], null=False)),
+            ('group', models.ForeignKey(orm[u'auth.group'], null=False))
         ))
-        db.create_unique('waffle_flag_groups', ['flag_id', 'group_id'])
+        db.create_unique(m2m_table_name, ['flag_id', 'group_id'])
 
         # Adding M2M table for field users on 'Flag'
-        db.create_table('waffle_flag_users', (
+        m2m_table_name = db.shorten_name(u'waffle_flag_users')
+        db.create_table(m2m_table_name, (
             ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
-            ('flag', models.ForeignKey(orm['waffle.flag'], null=False)),
-            ('user', models.ForeignKey(get_user_model(), null=False))
+            ('flag', models.ForeignKey(orm[u'waffle.flag'], null=False)),
+            ('user', models.ForeignKey(orm[getattr(settings, 'AUTH_USER_MODEL', 'auth.user')], null=False))
         ))
-        db.create_unique('waffle_flag_users', ['flag_id', 'user_id'])
+        db.create_unique(m2m_table_name, ['flag_id', 'user_id'])
 
         # Adding model 'Switch'
-        db.create_table('waffle_switch', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'waffle_switch', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=100)),
             ('active', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('note', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, db_index=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
         ))
-        db.send_create_signal('waffle', ['Switch'])
+        db.send_create_signal(u'waffle', ['Switch'])
+
+        # Adding model 'Sample'
+        db.create_table(u'waffle_sample', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=100)),
+            ('percent', self.gf('django.db.models.fields.DecimalField')(max_digits=4, decimal_places=1)),
+            ('note', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, db_index=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+        ))
+        db.send_create_signal(u'waffle', ['Sample'])
 
 
     def backwards(self, orm):
-        
         # Deleting model 'Flag'
-        db.delete_table('waffle_flag')
+        db.delete_table(u'waffle_flag')
 
         # Removing M2M table for field groups on 'Flag'
-        db.delete_table('waffle_flag_groups')
+        db.delete_table(db.shorten_name(u'waffle_flag_groups'))
 
         # Removing M2M table for field users on 'Flag'
-        db.delete_table('waffle_flag_users')
+        db.delete_table(db.shorten_name(u'waffle_flag_users'))
 
         # Deleting model 'Switch'
-        db.delete_table('waffle_switch')
+        db.delete_table(u'waffle_switch')
+
+        # Deleting model 'Sample'
+        db.delete_table(u'waffle_sample')
 
 
     models = {
-        'auth.group': {
+        u'auth.group': {
             'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
         },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
             'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
-        'auth.user': {
+        u'auth.user': {
             'Meta': {'object_name': 'User'},
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
         },
-        'contenttypes.contenttype': {
+        u'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        'waffle.flag': {
+        u'waffle.flag': {
             'Meta': {'object_name': 'Flag'},
             'authenticated': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True'}),
             'everyone': ('django.db.models.fields.NullBooleanField', [], {'null': 'True', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'languages': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'note': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'percent': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '3', 'decimal_places': '1', 'blank': 'True'}),
             'rollout': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'superusers': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'users': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.User']", 'symmetrical': 'False', 'blank': 'True'})
+            'testing': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'users': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.User']", 'symmetrical': 'False', 'blank': 'True'})
         },
-        'waffle.switch': {
+        u'waffle.sample': {
+            'Meta': {'object_name': 'Sample'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'note': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'percent': ('django.db.models.fields.DecimalField', [], {'max_digits': '4', 'decimal_places': '1'})
+        },
+        u'waffle.switch': {
             'Meta': {'object_name': 'Switch'},
             'active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'})
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '100'}),
+            'note': ('django.db.models.fields.TextField', [], {'blank': 'True'})
         }
     }
 

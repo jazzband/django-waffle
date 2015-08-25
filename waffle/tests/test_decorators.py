51dd5f61fb08e4fc0c9f202308af775ce5a068dc
Rodrigo de Paiva Gomes <rgomes@gmail.com>
2015-08-25 14:52:41 +0200

    Added parameter to decorator to enable user to redirect to different view (instead of 404) when using toggles on the views

--- waffle/tests/test_decorators.py
+++ waffle/tests/test_decorators.py
@@ -30,3 +30,45 @@ class DecoratorTests(TestCase):
         Switch.objects.create(name='foo', active=True)
         resp = self.client.get('/switch-off')
         self.assertEqual(404, resp.status_code)
+
+    def test_switch_must_be_inactive_and_redirect_to_view(self):
+        resp = self.client.get('/switched_view_with_valid_redirect')
+        self.assertEqual(302, resp.status_code)
+        Switch.objects.create(name='foo', active=True)
+        resp = self.client.get('/switched_view_with_valid_redirect')
+        self.assertEqual(200, resp.status_code)
+
+    def test_switch_must_be_inactive_and_redirect_to_named_view(self):
+        resp = self.client.get('/switched_view_with_valid_url_name')
+        self.assertEqual(302, resp.status_code)
+        Switch.objects.create(name='foo', active=True)
+        resp = self.client.get('/switched_view_with_valid_url_name')
+        self.assertEqual(200, resp.status_code)
+
+    def test_switch_must_be_inactive_and_not_redirect(self):
+        resp = self.client.get('/switched_view_with_invalid_redirect')
+        self.assertEqual(404, resp.status_code)
+        Switch.objects.create(name='foo', active=True)
+        resp = self.client.get('/switched_view_with_invalid_redirect')
+        self.assertEqual(200, resp.status_code)
+
+    def test_flag_must_be_inactive_and_redirect_to_view(self):
+        resp = self.client.get('/flagged_view_with_valid_redirect')
+        self.assertEqual(302, resp.status_code)
+        Flag.objects.create(name='foo', everyone=True)
+        resp = self.client.get('/flagged_view_with_valid_redirect')
+        self.assertEqual(200, resp.status_code)
+
+    def test_flag_must_be_inactive_and_redirect_to_named_view(self):
+        resp = self.client.get('/flagged_view_with_valid_url_name')
+        self.assertEqual(302, resp.status_code)
+        Flag.objects.create(name='foo', everyone=True)
+        resp = self.client.get('/flagged_view_with_valid_url_name')
+        self.assertEqual(200, resp.status_code)
+
+    def test_flag_must_be_inactive_and_not_redirect(self):
+        resp = self.client.get('/flagged_view_with_invalid_redirect')
+        self.assertEqual(404, resp.status_code)
+        Flag.objects.create(name='foo', everyone=True)
+        resp = self.client.get('/flagged_view_with_invalid_redirect')
+        self.assertEqual(200, resp.status_code)

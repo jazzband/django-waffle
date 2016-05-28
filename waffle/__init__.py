df6588e93ef8d40518d2d27325c6cb1d0b60dabb
James Socol <me@jamessocol.com>
2016-05-28 15:28:43 -0400

    Move most flag_is_active to Flag.is_active

--- waffle/__init__.py
+++ waffle/__init__.py
@@ -17,98 +17,11 @@ class DoesNotExist(object):
         return get_setting('SWITCH_DEFAULT')
 
 
-def set_flag(request, flag_name, active=True, session_only=False):
-    """Set a flag value on a request object."""
-    if not hasattr(request, 'waffles'):
-        request.waffles = {}
-    request.waffles[flag_name] = [active, session_only]
-
-
 def flag_is_active(request, flag_name):
-    from .models import cache_flag, Flag
-    cache = get_cache()
+    from .models import Flag
 
-    flag = cache.get(keyfmt(get_setting('FLAG_CACHE_KEY'), flag_name))
-    if flag is None:
-        try:
-            flag = Flag.objects.get(name=flag_name)
-            cache_flag(instance=flag)
-        except Flag.DoesNotExist:
-            return get_setting('FLAG_DEFAULT')
-
-    if get_setting('OVERRIDE'):
-        if flag_name in request.GET:
-            return request.GET[flag_name] == '1'
-
-    if flag.everyone:
-        return True
-    elif flag.everyone is False:
-        return False
-
-    if flag.testing:  # Testing mode is on.
-        tc = get_setting('TEST_COOKIE') % flag_name
-        if tc in request.GET:
-            on = request.GET[tc] == '1'
-            if not hasattr(request, 'waffle_tests'):
-                request.waffle_tests = {}
-            request.waffle_tests[flag_name] = on
-            return on
-        if tc in request.COOKIES:
-            return request.COOKIES[tc] == 'True'
-
-    user = request.user
-
-    if flag.authenticated and user.is_authenticated():
-        return True
-
-    if flag.staff and user.is_staff:
-        return True
-
-    if flag.superusers and user.is_superuser:
-        return True
-
-    if flag.languages:
-        languages = flag.languages.split(',')
-        if (hasattr(request, 'LANGUAGE_CODE') and
-                request.LANGUAGE_CODE in languages):
-            return True
-
-    flag_users = cache.get(keyfmt(get_setting('FLAG_USERS_CACHE_KEY'),
-                                              flag.name))
-    if flag_users is None:
-        flag_users = flag.users.all()
-        cache_flag(instance=flag)
-    if user in flag_users:
-        return True
-
-    flag_groups = cache.get(keyfmt(get_setting('FLAG_GROUPS_CACHE_KEY'),
-                                   flag.name))
-    if flag_groups is None:
-        flag_groups = flag.groups.all()
-        cache_flag(instance=flag)
-    user_groups = user.groups.all()
-    for group in flag_groups:
-        if group in user_groups:
-            return True
-
-    if flag.percent and flag.percent > 0:
-        if not hasattr(request, 'waffles'):
-            request.waffles = {}
-        elif flag_name in request.waffles:
-            return request.waffles[flag_name][0]
-
-        cookie = get_setting('COOKIE') % flag_name
-        if cookie in request.COOKIES:
-            flag_active = (request.COOKIES[cookie] == 'True')
-            set_flag(request, flag_name, flag_active, flag.rollout)
-            return flag_active
-
-        if Decimal(str(random.uniform(0, 100))) <= flag.percent:
-            set_flag(request, flag_name, True, flag.rollout)
-            return True
-        set_flag(request, flag_name, False, flag.rollout)
-
-    return False
+    flag = Flag.get(flag_name)
+    return flag.is_active(request)
 
 
 def switch_is_active(switch_name):

014385974d3c37ff9611db16e7325c8ef7ac1437
Sławomir Ehlert <slafs.e@gmail.com>
2019-02-24 21:40:29 +0100

    Passing decorator effects to child classes (#331)
    When using `waffle.testutils.override_*` decorators on a class one would expect the change to be visible on child classes too (i.e. inheritance would work as normal).
    
    This change drops the rather hacky implementation of `testutils._overrider.for_class` method in favor of Django's `django.test.utils.TestContextDecorator` (the same one that's used as a base for `override_settings` utility).
    
    The most notable change here is that `TestContextDecorator` hooks up with unittest's `setUp`/`tearDown` mechanism instead of decorating each test method in a decorated class.
    
    **Backwards incompatible change (!!!):**
    Only subclasses of `unittest.case.TestCase` can be decorated with `testutils.override_*` decorators now. This means for example that mixin-like test classes (inheriting from `object` only) can't be decorated like that anymore.

--- waffle/testutils.py
+++ waffle/testutils.py
@@ -1,57 +1,19 @@
 from __future__ import unicode_literals
 
-import sys
-import types
-from functools import wraps
+from django.test.utils import TestContextDecorator
 
 from waffle import get_waffle_flag_model
 from waffle.models import Switch, Sample
 
 
 __all__ = ['override_flag', 'override_sample', 'override_switch']
-PY3 = sys.version_info[0] == 3
-if PY3:
-    CLASS_TYPES = (type,)
-else:
-    CLASS_TYPES = (type, types.ClassType)
 
 
-class _overrider(object):
+class _overrider(TestContextDecorator):
     def __init__(self, name, active):
+        super(_overrider, self).__init__()
         self.name = name
         self.active = active
-
-    def __call__(self, func):
-        if isinstance(func, CLASS_TYPES):
-            return self.for_class(func)
-        else:
-            return self.for_callable(func)
-
-    def for_class(self, obj):
-        """Wraps a class's test methods in the decorator"""
-        for attr in dir(obj):
-            if not attr.startswith('test_'):
-                # Ignore non-test functions
-                continue
-
-            attr_value = getattr(obj, attr)
-
-            if not callable(attr_value):
-                # Ignore non-functions
-                continue
-
-            setattr(obj, attr, self.for_callable(attr_value))
-
-        return obj
-
-    def for_callable(self, func):
-        """Wraps a method in the decorator"""
-        @wraps(func)
-        def _wrapped(*args, **kwargs):
-            with self:
-                return func(*args, **kwargs)
-
-        return _wrapped
 
     def get(self):
         self.obj, self.created = self.cls.objects.get_or_create(name=self.name)
@@ -62,13 +24,13 @@ class _overrider(object):
     def get_value(self):
         raise NotImplementedError
 
-    def __enter__(self):
+    def enable(self):
         self.get()
         self.old_value = self.get_value()
         if self.old_value != self.active:
             self.update(self.active)
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def disable(self):
         if self.created:
             self.obj.delete()
             self.obj.flush()

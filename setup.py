f1a3ccb62d5755d6f1371b14784c73bb5a311263
Clinton Blackburn <clinton.blackburn@gmail.com>
2019-07-01 10:46:40 -0700

    Releasing v0.17.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-waffle',
-    version='0.16.0',
+    version='0.17.0',
     description='A feature flipper for Django.',
     long_description=open('README.rst').read(),
     author='James Socol',

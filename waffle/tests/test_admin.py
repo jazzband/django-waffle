3d5add717e8746b3649d852806b05c4fb293e319
Petter Strandmark <petter.strandmark@gmail.com>
2019-10-23 06:21:31 +0200

    Add log entries for the admin panel quick links. (#349)

--- waffle/tests/test_admin.py
+++ waffle/tests/test_admin.py
@@ -4,10 +4,13 @@ try:
 except ImportError:
     import unittest.mock as mock
 
+from django.contrib.admin.models import LogEntry, CHANGE, DELETION
 from django.contrib.admin.sites import AdminSite
 from django.contrib.auth import get_user_model
 from waffle import get_waffle_flag_model
-from waffle.admin import FlagAdmin, InformativeManyToManyRawIdWidget
+from waffle.admin import (FlagAdmin, InformativeManyToManyRawIdWidget, enable_for_all,
+                          disable_for_all, delete_individually, enable_switches, disable_switches)
+from waffle.models import Switch
 from waffle.tests.base import TestCase
 
 
@@ -16,12 +19,19 @@ class FakeSuperUser:
         return True
 
 
+class FakeRequest:
+    def __init__(self):
+        self.user = get_user_model().objects.create(username="test1")
+
+
+Flag = get_waffle_flag_model()
+
+
 class FlagAdminTests(TestCase):
     def setUp(self):
         self.site = AdminSite()
 
     def test_informative_widget(self):
-        Flag = get_waffle_flag_model()
         flag_admin = FlagAdmin(Flag, self.site)
 
         request = mock.Mock()
@@ -34,3 +44,61 @@ class FlagAdminTests(TestCase):
         user2 = get_user_model().objects.create(username="test2")
         self.assertIn("(test1, test2)",
                       user_widget.render("users", [user1.pk, user2.pk]))
+
+    def test_enable_for_all(self):
+        f1 = Flag.objects.create(name="flag1", everyone=False)
+
+        request = FakeRequest()
+        enable_for_all(None, request, Flag.objects.all())
+
+        f1.refresh_from_db()
+        self.assertTrue(f1.everyone)
+        log_entry = LogEntry.objects.get(user=request.user)
+        self.assertEqual(log_entry.action_flag, CHANGE)
+        self.assertEqual(log_entry.object_repr, "flag1 on")
+
+    def test_disable_for_all(self):
+        f1 = Flag.objects.create(name="flag1", everyone=True)
+
+        request = FakeRequest()
+        disable_for_all(None, request, Flag.objects.all())
+
+        f1.refresh_from_db()
+        self.assertFalse(f1.everyone)
+        log_entry = LogEntry.objects.get(user=request.user)
+        self.assertEqual(log_entry.action_flag, CHANGE)
+        self.assertEqual(log_entry.object_repr, "flag1 off")
+
+    def test_delete_individually(self):
+        Flag.objects.create(name="flag1", everyone=True)
+
+        request = FakeRequest()
+        delete_individually(None, request, Flag.objects.all())
+
+        self.assertIsNone(Flag.objects.first())
+        log_entry = LogEntry.objects.get(user=request.user)
+        self.assertEqual(log_entry.action_flag, DELETION)
+
+    def test_enable_switches(self):
+        s1 = Switch.objects.create(name="switch1", active=False)
+
+        request = FakeRequest()
+        enable_switches(None, request, Switch.objects.all())
+
+        s1.refresh_from_db()
+        self.assertTrue(s1.active)
+        log_entry = LogEntry.objects.get(user=request.user)
+        self.assertEqual(log_entry.action_flag, CHANGE)
+        self.assertEqual(log_entry.object_repr, "switch1 on")
+
+    def test_disable_switches(self):
+        s1 = Switch.objects.create(name="switch1", active=True)
+
+        request = FakeRequest()
+        disable_switches(None, request, Switch.objects.all())
+
+        s1.refresh_from_db()
+        self.assertFalse(s1.active)
+        log_entry = LogEntry.objects.get(user=request.user)
+        self.assertEqual(log_entry.action_flag, CHANGE)
+        self.assertEqual(log_entry.object_repr, "switch1 off")

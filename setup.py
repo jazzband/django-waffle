439409fcd6b280c4085b9b1e66428514a2a2337e
Clinton Blackburn <clinton.blackburn@gmail.com>
2020-09-02 17:24:40 -0700

    Corrected classifier

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'Framework :: Django :: 3.1',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: only',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',

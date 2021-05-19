50b0beb0c72c7e048d9878d259405e35554fa21d
Caitlin <caitlin.white@commoncode.io>
2021-05-19 18:41:18 +1000

    Add support for Django 3.2
    - Configure default auto field for waffle and test_app - Django 3.2 adds
      the ability to customise the type of auto-created primary key fields.
    - Add Django 3.2 to tox.ini and setup.py

--- waffle/apps.py
+++ waffle/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 class WaffleConfig(AppConfig):
     name = 'waffle'
     verbose_name = 'django-waffle'
+    default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
         import waffle.signals  # noqa: F401

161470ae2c8f2e2be1bff211c8cb314453506478
Michał Bielawski <michal.bielawski@apperio.com>
2021-07-30 11:32:38 +0100

    Add python 3.9 to classifiers

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )

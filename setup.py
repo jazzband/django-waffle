770ae4b0ad3f8085480c8dc48782bfe4be9b41b0
Clinton Blackburn <clinton.blackburn@gmail.com>
2022-07-19 22:23:45 -0700

    Releasing 2.6.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-waffle',
-    version='2.5.0',
+    version='2.6.0',
     description='A feature flipper for Django.',
     long_description=open('README.rst').read(),
     author='James Socol',

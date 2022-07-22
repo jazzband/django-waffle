b8ec135368f18b1f0aadc1b1ed5c035b817c1a2f
Shaun Gallagher <shaun@encantos.co>
2022-07-22 06:46:39 -0400

    Add view that returns Waffle flag/switch/sample status as JSON

--- waffle/views.py
+++ waffle/views.py
@@ -1,9 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from django.views.decorators.cache import never_cache
 
-from waffle import get_waffle_flag_model
-from waffle.models import Sample, Switch
+from waffle import get_waffle_flag_model, get_waffle_switch_model, get_waffle_sample_model
 from waffle.utils import get_setting
 
 
@@ -17,10 +16,10 @@ def _generate_waffle_js(request):
     flags = get_waffle_flag_model().get_all()
     flag_values = [(f.name, f.is_active(request)) for f in flags]
 
-    switches = Switch.get_all()
+    switches = get_waffle_switch_model().get_all()
     switch_values = [(s.name, s.is_active()) for s in switches]
 
-    samples = Sample.get_all()
+    samples = get_waffle_sample_model().get_all()
     sample_values = [(s.name, s.is_active()) for s in samples]
 
     return loader.render_to_string('waffle/waffle.js', {
@@ -31,3 +30,46 @@ def _generate_waffle_js(request):
         'switch_default': get_setting('SWITCH_DEFAULT'),
         'sample_default': get_setting('SAMPLE_DEFAULT'),
     })
+
+
+@never_cache
+def waffle_json(request):
+    return JsonResponse(_generate_waffle_json(request))
+
+
+def _generate_waffle_json(request):
+    flags = get_waffle_flag_model().get_all()
+    flag_values = [
+        {
+            'name': f.name,
+            'is_active': f.is_active(request),
+            'last_modified': f.modified,
+        }
+        for f in flags
+    ]
+
+    switches = get_waffle_switch_model().get_all()
+    switch_values = [
+        {
+            'name': s.name,
+            'is_active': s.is_active(),
+            'last_modified': s.modified,
+        }
+        for s in switches
+    ]
+
+    samples = get_waffle_sample_model().get_all()
+    sample_values = [
+        {
+            'name': s.name,
+            'is_active': s.is_active(),
+            'last_modified': s.modified,
+        }
+        for s in samples
+    ]
+
+    return {
+        'flags': flag_values,
+        'switches': switch_values,
+        'samples': sample_values,
+    }
